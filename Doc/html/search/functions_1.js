baff9ce26227e9ff27424465833fbce9b1760db9
Micro9261 <140260920+Micro9261@users.noreply.github.com>
2025-12-20 12:50:03 +0100

    Doxygen generation Reader newest data indicator and fix

--- Doc/html/search/functions_1.js
+++ Doc/html/search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['clear_0',['Clear',['../class_recording_vector.html#acb7e09fda9bd472415b1c1f75a61cc42',1,'RecordingVector::Clear()'],['../class_recording_history.html#a76b5f354445a37b7e0bc42a977422e32',1,'RecordingHistory::Clear()']]],
-  ['create_5ffunc_1',['Create_Func',['../namespace_dummy.html#acf86e7ee3184771b7ea3a373aa590f8e',1,'Dummy']]]
+  ['check_5fif_5fnew_5fdata_5floaded_0',['Check_if_new_data_loaded',['../class_file_reader.html#a4cbbd5cc04696298fa9dcd9830981335',1,'FileReader::Check_if_new_data_loaded()'],['../class_i_reader.html#a6907c4ecf8dead829b083b4435177a83',1,'IReader::Check_if_new_data_loaded()']]],
+  ['clear_1',['Clear',['../class_recording_vector.html#acb7e09fda9bd472415b1c1f75a61cc42',1,'RecordingVector::Clear()'],['../class_recording_history.html#a76b5f354445a37b7e0bc42a977422e32',1,'RecordingHistory::Clear()']]],
+  ['create_5ffunc_2',['Create_Func',['../namespace_dummy.html#acf86e7ee3184771b7ea3a373aa590f8e',1,'Dummy']]]
 ];
